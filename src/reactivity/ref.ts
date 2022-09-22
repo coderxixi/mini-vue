b26d6e883fd0cf9d39ff3acfbb78bcf5898a3587
liuss7 <liuss7@asiainfo.com>
2022-09-22 21:02:35 +0800

    isRef unRef 功能的实现

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -5,6 +5,7 @@ class RefImpl {
   private _value: any;
   public dep;
   private _rawValue: any;
+  public __v_isRef=true;
   constructor(value) {
     this._rawValue = value
     //判断value是不是对象 
@@ -28,4 +29,11 @@ class RefImpl {
 }
 export function ref(value) {
   return new RefImpl(value)
+}
+export function isRef(ref){
+  return !!ref.__v_isRef;
+}
+
+export function unRef(ref){
+  return isRef(ref)?ref.value:ref
 }
